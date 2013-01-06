418b8634316713d3d0dea3b1d7b01e981884e821
James Michael DuPont <jamesmikedupont@gmail.com>
2013-01-06 10:15:27 -0600

    starting to organize the output

--- edelivery.pl
+++ edelivery.pl
@@ -171,18 +171,30 @@ sub download_aru {
     my $ua=shift;
     my $dir=shift;
     my $data=shift;
-
-    
-    while ($data =~ m/(EPD\/Down[^\"\']+)[\"\']/gm){
-	Download $ua, $dir, 'https://edelivery.oracle.com/' . $1;
-    }
-    
-    while ($data =~ m/(EPD\/ViewDigest[^\"\']+)[\"\']/gm){
-	Download $ua, $dir, 'https://edelivery.oracle.com/' . $1;
-    }
     
 }
 
+
+sub output_aru_list 
+{
+    my $ua=shift;
+    my $data=shift;
+    my $productno =shift;
+    my $platformno=shift;
+
+    mkdir "output" unless -d "output";
+    mkdir "output/$productno" unless -d "output/$productno";
+    mkdir "output/$productno/$platformno" unless -d "output/$productno/$platformno";
+
+    open OUT, ">output/$productno/$platformno/packages_aru.html";
+    print OUT $data;
+    close OUT;
+    my $form = HTML::Form->parse($data, 'https://edelivery.oracle.com');
+    open OUT, ">output/$productno/$platformno/packagesaru.pm";
+    print OUT Dumper($form->inputs);;
+    close OUT;
+
+}
 
 sub packages_aru
 {
@@ -192,15 +204,8 @@ sub packages_aru
     my $platformno=shift;
     my $aruno=shift;
 
-    open OUT, ">packages_aru.html";
-    print OUT $data;
-    close OUT;
-
     my $form = HTML::Form->parse($data, 'https://edelivery.oracle.com');
-
-    open OUT, ">packagesaru.pm";
-    print OUT Dumper($form->inputs);;
-    close OUT;
+## now fill out the form
 
     my $product=$form->find_input('product');
     die unless $product;
@@ -213,15 +218,24 @@ sub packages_aru
 
     # set the aru number
     my $aru=$form->find_input('egroup_aru_number');
-
     $aru->value($aruno); 
-
 
     my $res = $ua->request($form->click);
     $data = $res->content();
 
+    return $data;
+}
 
-    open OUT, ">packagesaruout.html";
+sub download_aru 
+{
+
+    my $ua=shift;
+    my $data=shift;
+    my $productno =shift;
+    my $platformno=shift;
+    my $aruno=shift;
+    
+    open OUT, ">output/$productno/$platformno/$aruno/packagesaruout.html";
     print OUT $data;
     close OUT;
 
@@ -230,7 +244,15 @@ sub packages_aru
     mkdir "output/$productno/$platformno" unless -d "output/$productno/$platformno";
     mkdir "output/$productno/$platformno/$aruno" unless -d "output/$productno/$platformno/$aruno";
 
-    download_aru $ua, "output/$productno/$platformno/$aruno", $data; # now run the downloads
+
+    my $dir="output/$productno/$platformno/$aruno";
+    while ($data =~ m/(EPD\/Down[^\"\']+)[\"\']/gm){
+	Download $ua, $dir, 'https://edelivery.oracle.com/' . $1;
+    }
+    
+    while ($data =~ m/(EPD\/ViewDigest[^\"\']+)[\"\']/gm){
+	Download $ua, $dir, 'https://edelivery.oracle.com/' . $1;
+    }
 
 }
 
@@ -254,10 +276,14 @@ $ua->agent('Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; .NET CLR 1.1.4322
 #$ua->add_handler("request_send",  sub { shift->dump; return });
 #$ua->add_handler("response_done", sub { shift->dump; return });
 
+my $product_no = 10120; #'name' => 'Oracle Fusion Middleware',                             'value' => '10120',
+my $platform_no = 226; #'linux 64
 my $welcome = getWelcome $ua, 'https://edelivery.oracle.com';
 my $export   = login $ua, $welcome;
 my $packages = exportAgree $ua,$export;
-my $paru = packages $ua, $packages,10120, 226;
+my $paru = packages $ua, $packages,$product_no, $platform_no;
+
+
 
 
 #product
@@ -265,9 +291,14 @@ my $paru = packages $ua, $packages,10120, 226;
 # prod '10120'
 # plat '226' # linux 64
 # aru '15737734' # Oracle WebLogic Server 12c Media Pack
+my $aru_no=15737734;
 
-packages_aru $ua, $paru, 10120, 226, 15737734;
+output_aru_list $ua, $paru, $product_no, $platform_no; # this saves the list of arus to the file, we will make a menu
 
+#get the list of files you can download, for the aru
+my $aru_list = packages_aru $ua, $paru, $product_no, $platform_no, $aru_no;
+
+download_aru  $ua, $aru_list, $product_no, $platform_no, $aru_no;
 
 # a list of packages, now we need to select one to get the downloads
 # https://edelivery.oracle.com/EPD/Search/handle_go
